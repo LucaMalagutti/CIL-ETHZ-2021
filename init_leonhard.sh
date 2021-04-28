4b8c51b3a956c5a302ed1c56e018a362007af94a
Luca Malagutti <malagutti.luca@yahoo.it>
2021-04-28 17:28:28 +0200

    Updated pre-commit hooks config

--- init_leonhard.sh
+++ init_leonhard.sh
@@ -1,4 +1,4 @@
-echo "Init Leonhard"
+echo "Init Leonhard..."
 module load gcc/6.3.0 python_gpu/3.7.4
 source bin/activate
-pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+pip install -q -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
