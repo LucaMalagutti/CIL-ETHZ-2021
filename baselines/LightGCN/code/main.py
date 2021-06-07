53a80f3c426d551c40f4f087f2a43d117b7f4e84
LucaMalagutti <malagutti.luca@yahoo.it>
2021-06-07 18:15:21 +0100

    init LightGCN

---
+++ baselines/LightGCN/code/main.py
@@ -0,0 +1,57 @@
+import time
+from os.path import join
+
+import numpy as np
+import Procedure
+import torch
+import utils
+import world
+from tensorboardX import SummaryWriter
+from world import cprint
+
+# ==============================
+utils.set_seed(world.seed)
+print(">>SEED:", world.seed)
+# ==============================
+import register
+from register import dataset
+
+Recmodel = register.MODELS[world.model_name](world.config, dataset)
+Recmodel = Recmodel.to(world.device)
+bpr = utils.BPRLoss(Recmodel, world.config)
+
+weight_file = utils.getFileName()
+print(f"load and save to {weight_file}")
+if world.LOAD:
+    try:
+        Recmodel.load_state_dict(
+            torch.load(weight_file, map_location=torch.device("cpu"))
+        )
+        world.cprint(f"loaded model weights from {weight_file}")
+    except FileNotFoundError:
+        print(f"{weight_file} not exists, start from beginning")
+Neg_k = 1
+
+# init tensorboard
+if world.tensorboard:
+    w: SummaryWriter = SummaryWriter(
+        join(world.BOARD_PATH, time.strftime("%m-%d-%Hh%Mm%Ss-") + "-" + world.comment)
+    )
+else:
+    w = None
+    world.cprint("not enable tensorflowboard")
+
+try:
+    for epoch in range(world.TRAIN_epochs):
+        start = time.time()
+        if epoch % 10 == 0:
+            cprint("[TEST]")
+            Procedure.Test(dataset, Recmodel, epoch, w, world.config["multicore"])
+        output_information = Procedure.BPR_train_original(
+            dataset, Recmodel, bpr, epoch, neg_k=Neg_k, w=w
+        )
+        print(f"EPOCH[{epoch+1}/{world.TRAIN_epochs}] {output_information}")
+        torch.save(Recmodel.state_dict(), weight_file)
+finally:
+    if world.tensorboard:
+        w.close()
