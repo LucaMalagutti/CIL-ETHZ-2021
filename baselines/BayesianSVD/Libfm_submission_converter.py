40452d54fdd72c0888b6d2697d0d22a5ec73ca2a
FedericoGossi <federico.gossi@mail.polimi.it>
2021-06-29 23:45:38 +0200

    Bayesian SVD++ (https://arxiv.org/pdf/1905.01395v1.pdf) instructions and data converter for libfm (http://www.libfm.org/)

---
+++ baselines/BayesianSVD/Libfm_submission_converter.py
@@ -0,0 +1,16 @@
+pred_file = "BayesianSVDpp_predictions.csv"
+samplesub_file = "/home/ico/PycharmProjects/CIL-2021/data/sample_submission.csv"
+libfm_file = "/home/ico/PycharmProjects/CIL-2021/baselines/DeepRec/data/submission/libfmPrediction"
+
+with open(libfm_file) as predf:
+    with open(pred_file, "w") as outf:
+        with open(samplesub_file) as samplef:
+            outf.write("Id,Prediction\n")
+            line_num = 0
+            for pred_line, sample_line in zip(predf.readlines(), samplef.readlines()[1:]):
+                user_item = sample_line.split(",")[0]
+                rating = float(pred_line)
+                outf.write(f"{user_item},{rating}\n")
+                line_num += 1
+                if line_num % 100000 == 0:
+                    print(line_num)
