932829431d48ae2f833f5392d49c00d7c4c42c6a
Antonio Lopardo <antonio.lopardo@outlook.com>
2021-07-16 15:36:36 +0200

    add DeepRec++ model
    Co-Authored-By: Luca Malagutti <1375207+LucaMalagutti@users.noreply.github.com>

---
+++ DeepRec++/extract_embeddings.py
@@ -0,0 +1,189 @@
+# Copyright (c) 2017 NVIDIA Corporation
+import argparse
+import copy
+import json
+import pickle
+import random
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from data_utils.CIL_data_converter import convert2CILdictionary
+from reco_encoder.data import input_layer
+from reco_encoder.model import model
+from torch.autograd import Variable
+
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+
+parser = argparse.ArgumentParser(description="RecoEncoder")
+
+parser.add_argument(
+    "--drop_prob", type=float, default=0.0, metavar="N", help="dropout drop probability"
+)
+parser.add_argument(
+    "--constrained", action="store_true", help="constrained autoencoder"
+)
+parser.add_argument(
+    "--skip_last_layer_nl",
+    action="store_true",
+    help="if present, decoder's last layer will not apply non-linearity function",
+)
+parser.add_argument(
+    "--hidden_layers",
+    type=str,
+    default="1024,512,512,128",
+    metavar="N",
+    help="hidden layer sizes, comma-separated",
+)
+parser.add_argument(
+    "--path_to_train_data",
+    type=str,
+    default="data/train90",
+    metavar="N",
+    help="Path to training data",
+)
+parser.add_argument(
+    "--path_to_eval_data",
+    type=str,
+    default="data/valid",
+    metavar="N",
+    help="Path to evaluation data",
+)
+parser.add_argument(
+    "--non_linearity_type",
+    type=str,
+    default="selu",
+    metavar="N",
+    help="type of the non-linearity used in activations",
+)
+parser.add_argument(
+    "--save_path",
+    type=str,
+    default="autorec.pt",
+    metavar="N",
+    help="where to saved model",
+)
+parser.add_argument(
+    "--predictions_path",
+    type=str,
+    default="out.json",
+    metavar="N",
+    help="where to save predictions",
+)
+parser.add_argument(
+    "--major",
+    type=str,
+    default="users",
+    metavar="N",
+    help="produce user-based or item-based autoencoder",
+)
+
+args = parser.parse_args()
+print(args)
+
+use_gpu = torch.cuda.is_available()  # global flag
+if use_gpu:
+    print("GPU is available.")
+else:
+    print("GPU is not available.")
+
+
+def main():
+    params = dict()
+    params["batch_size"] = 1
+    params["data_dir"] = args.path_to_train_data
+    params["major"] = args.major
+    params["itemIdInd"] = 1
+    params["userIdInd"] = 0
+    print("Loading training data")
+    data_layer = input_layer.UserItemRecDataProvider(params=params)
+    print("Data loaded")
+    print("Total items found: {}".format(len(data_layer.data.keys())))
+    print("Vector dim: {}".format(data_layer.vector_dim))
+
+    print("Loading submission data")
+    eval_params = copy.deepcopy(params)
+    # must set eval batch size to 1 to make sure no examples are missed
+    eval_params["batch_size"] = 1
+    eval_params["data_dir"] = args.path_to_eval_data
+    eval_data_layer = input_layer.UserItemRecDataProvider(
+        params=eval_params,
+        user_id_map=data_layer.userIdMap,
+        item_id_map=data_layer.itemIdMap,
+    )
+    print("Total submission items found: {}".format(len(eval_data_layer.data.keys())))
+    print("Vector dim: {}".format(eval_data_layer.vector_dim))
+
+    rencoder = model.AutoEncoder(
+        layer_sizes=[data_layer.vector_dim]
+        + [int(l_sizes) for l_sizes in args.hidden_layers.split(",")],
+        nl_type=args.non_linearity_type,
+        is_constrained=args.constrained,
+        dp_drop_prob=args.drop_prob,
+        last_layer_activations=not args.skip_last_layer_nl,
+        extract_embeddings=True,
+    )
+
+    path_to_model = Path(args.save_path)
+    if path_to_model.is_file():
+        print("Loading model from: {}".format(path_to_model))
+        rencoder.load_state_dict(torch.load(args.save_path))
+
+    print("######################################################")
+    print("######################################################")
+    print("############# AutoEncoder Model: #####################")
+    print(rencoder)
+    print("######################################################")
+    print("######################################################")
+
+    rencoder.eval()
+
+    if use_gpu:
+        rencoder = rencoder.cuda()
+
+    inv_userIdMap = {v: k for k, v in data_layer.userIdMap.items()}
+    inv_itemIdMap = {v: k for k, v in data_layer.itemIdMap.items()}
+
+    eval_data_layer.src_data = data_layer.data
+
+    embeddings_dict = dict()
+
+    for i, ((out, src), majorInd) in enumerate(
+        eval_data_layer.iterate_one_epoch_eval(for_inf=True)
+    ):
+
+        # input for the autoencoder
+        major_ratings = Variable(src.cuda().to_dense() if use_gpu else src.to_dense())
+
+        # rencoder is built to output the deep features
+        embedding = rencoder(major_ratings).cpu().data.numpy()[0, :]
+
+        # get user idx as in the train data
+        if args.major == "users":
+            major_idx = inv_userIdMap[majorInd]
+        else:
+            major_idx = inv_itemIdMap[majorInd]
+
+        if major_idx not in embeddings_dict:
+            if major_idx == 955:
+                print(major_idx)
+
+            # new user: save deep features in the dictionary
+            embeddings_dict[int(major_idx)] = embedding.tolist()
+
+            if i % 500 == 0:
+                print("Extracted deep features for {} major".format(i))
+
+    print("Saving embedding dictionary..")
+    print(len(embeddings_dict))
+    with open(args.predictions_path, "wb") as outf:
+        pickle.dump(embeddings_dict, outf)
+
+    print("Saved embedding dictionary to:", args.predictions_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ DeepRec++/infer_mlp.py
@@ -0,0 +1,130 @@
+import argparse
+import copy
+import random
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from data_utils.CIL_data_converter import convert2CILdictionary
+from reco_encoder.data import input_layer
+from reco_encoder.model import model
+from run_mlp import EmbeddingDataset
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+
+parser = argparse.ArgumentParser(description="RecoEncoder")
+
+parser.add_argument(
+    "--dropout", type=float, default=0.0, metavar="N", help="dropout drop probability"
+)
+parser.add_argument(
+    "--input_size",
+    type=int,
+    default="128",
+    metavar="N",
+    help="size of the input",
+)
+parser.add_argument(
+    "--layer_sizes",
+    type=str,
+    default="64,32,16",
+    metavar="N",
+    help="hidden layer sizes",
+)
+parser.add_argument(
+    "--path_to_user_embs",
+    type=str,
+    default="data/embs/users_emb.pckl",
+    metavar="N",
+    help="Path to user emb",
+)
+
+parser.add_argument(
+    "--path_to_item_embs",
+    type=str,
+    default="data/embs/items_emb.pckl",
+    metavar="N",
+    help="Path to item emb",
+)
+parser.add_argument(
+    "--path_to_sub_data",
+    type=str,
+    default="data/submission/CIL_data.submission",
+    metavar="N",
+    help="Path to submission data",
+)
+parser.add_argument(
+    "--logdir",
+    type=str,
+    default="model_save/mlp/",
+    metavar="N",
+    help="where to load model from",
+)
+parser.add_argument(
+    "--predictions_path",
+    type=str,
+    default="mlp_out.csv",
+    metavar="N",
+    help="where to save predictions",
+)
+
+args = parser.parse_args()
+print(args)
+
+use_gpu = torch.cuda.is_available()  # global flag
+if use_gpu:
+    print("GPU is available.")
+else:
+    print("GPU is not available.")
+
+
+def main():
+
+    args.layer_sizes = [int(size) for size in args.layer_sizes.split(",")]
+
+    mlp = model.MLP(args.input_size, args.layer_sizes, args.dropout)
+
+    path_to_model = Path(args.logdir)
+    # if path_to_model.is_file():
+    print("Loading model from: {}".format(path_to_model))
+    mlp.load_state_dict(torch.load(args.logdir))
+
+    if use_gpu:
+        mlp.cuda()
+
+    mlp.eval()
+
+    sub_dataset = EmbeddingDataset(
+        args.path_to_user_embs, args.path_to_item_embs, args.path_to_sub_data
+    )
+    sub_dataloader = DataLoader(sub_dataset, batch_size=1024)
+
+    preds = []
+    for x, _ in tqdm(sub_dataloader):
+        pred_batch = mlp(x)
+        preds.extend([cur.item() for cur in pred_batch])
+
+    _COL_NAMES = ["user_id", "item_id", "rating"]
+    sub_data = pd.read_csv(args.path_to_sub_data, delimiter="\t", names=_COL_NAMES)
+
+    sub_data["Prediction"] = preds
+    sub_data["Id"] = (
+        "r"
+        + sub_data["user_id"].apply(lambda x: str(x))
+        + "_"
+        + "c"
+        + sub_data["item_id"].apply(lambda x: str(x))
+    )
+    sub_data = sub_data[["Id", "Prediction"]]
+
+    sub_data.to_csv(args.predictions_path, index=False)
+
+
+if __name__ == "__main__":
+    main()
